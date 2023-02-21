08b69d55447a49d99a215509cde978f96f5d90c2
itu-itis21-isike20 <ITU_isike20@itu.edu.tr>
2023-02-21 14:55:07 +0300

    added the game 'guess the number'

---
+++ Guess-The-Number-Project#1/script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNum = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+//document.querySelector('.number').textContent = secretNum;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //when there is no input
+  if (!guess || guess < 1 || guess > 20) {
+    displayMessage('Not in range!');
+  }
+  //when the guess is right
+  else if (guess === secretNum) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNum;
+    document.querySelector('body').style.backgroundColor = 'rgb(7, 199, 7)';
+    document.querySelector('.number').style.width = '30rem';
+    //setTimeout(function () {
+    //  document.querySelector('body').style.backgroundColor = '#222';
+    //}, 500);
+    if (score > Number(document.querySelector('.highscore').textContent)) {
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('body').style.backgroundColor = 'rgb(189, 0, 0)';
+      setTimeout(function () {
+        document.querySelector('body').style.backgroundColor = '#222';
+      }, 750);
+      if (guess < secretNum) {
+        if (secretNum - guess < 2) {
+          displayMessage('Low!');
+        } else {
+          displayMessage('Too low!');
+        }
+      } else if (guess > secretNum) {
+        if (guess - secretNum < 2) {
+          displayMessage('High!');
+        } else {
+          document.querySelector('.message').textContent = 'Too high!';
+          displayMessage('Too high!');
+        }
+      }
+    } else {
+      displayMessage('You lost the game!');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  score = 20;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  secretNum = Math.trunc(Math.random() * 20 + 1);
+});
